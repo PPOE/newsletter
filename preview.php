9702ed136b556924b9534502b487c63f59259cd9
desertrold <desertrold@piratenpartei.at>
2016-11-27 17:14:44 +0100

    Update preview.php

--- preview.php
+++ preview.php
@@ -263,7 +263,7 @@ echo '
       </div><!--/row-->
 
       <footer>
-        <p>Piratenpartei Österreichs, Schadinagasse 3, 1170 Wien</p>
+       <a href="https://wiki.piratenpartei.at/wiki/Piratenwiki:Impressum">Piratenpartei Österreichs - Impressum</a>
       </footer>
 
     </div><!--/.fluid-container-->
