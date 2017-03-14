7e3b00092c2c8b7d4f6624e8b985f84fb3989acc
zevenom <zevenom@gmail.com>
2017-03-14 20:22:02 +0100

    Impressum aktualisiert

--- create.php
+++ create.php
@@ -161,7 +161,7 @@ echo '
       </div><!--/row-->
 
       <footer>
-        <p>Piratenpartei Österreichs, Radetzkystraße 3/1, 8011 Graz</p>
+        <p><a href="https://wiki.piratenpartei.at/wiki/Piratenwiki:Impressum">Impressum</a></p>
       </footer>
 
     </div><!--/.fluid-container-->
